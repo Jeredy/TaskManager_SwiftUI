8fc224d46cc7f3b5b1e5b7946602d24a4b7c3eba
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-04 19:22:46 -0300

    Initial Commit

---
+++ TaskManager_SwiftUI/TaskManager_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManager_SwiftUIApp.swift
+//  TaskManager_SwiftUI
+//
+//  Created by André Almeida on 2022-07-04.
+//
+
+import SwiftUI
+
+@main
+struct TaskManager_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
